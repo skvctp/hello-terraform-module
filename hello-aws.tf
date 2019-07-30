98bd170127b6cb257b97c011d45c2c9fc7f7a351
Stephen Fox <5599297+stevefox@users.noreply.github.com>
2019-07-30 14:32:41 -0500

    Add optional and required arguments

--- hello-aws.tf
+++ hello-aws.tf
@@ -1,11 +1,15 @@
 // This is an AWS provider block
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 // This is an AWS resource block that creates an EC2 instance
 resource "aws_instance" "example" {
   ami           = "ami-2757f631"
   instance_type = "t2.${var.size}"
-}
 
+  tags = {
+    User = "${var.user_id}"
+    Repository = "github.com/cloudtp/hello-terraform-module"
+  }
+}
--- vars.tf
+++ vars.tf
@@ -1,3 +1,8 @@
 variable size {
-  description = "instance size"
+  description = "Instance size (type will be t2)."
+  default     = "micro"
+}
+
+variable user_id {
+  description = "Mandatory argument which is used to tag the EC2"
 }
